9b5a341438a0bee35759e6b114775484721cc8f3
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-11-10 11:45:29 +0600

    feat: add navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import BuyCredit from "./pages/BuyCredit";
 import Home from "./pages/Home";
 import Result from "./pages/Result";
-import {Routes, Route} from 'react-router-dom';
 
 const App = () => {
    return (
-      <Routes>
-         <Route path="/" element={<Home />} />
-         <Route path="/result" element={<Result />} />
-         <Route path="/buy" element={<BuyCredit />} />
-      </Routes>
+      <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50">
+         <Navbar />
+         <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/result" element={<Result />} />
+            <Route path="/buy" element={<BuyCredit />} />
+         </Routes>
+      </div>
    );
 };
 
