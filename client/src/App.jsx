2eb24ff88c09237155cc68470e8de6edc01741fd
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-11-10 11:19:54 +0600

    feat: add routes to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import BuyCredit from "./pages/BuyCredit";
 import Home from "./pages/Home";
 import Result from "./pages/Result";
+import {Routes, Route} from 'react-router-dom';
 
 const App = () => {
    return (
-      <div>
-         <Home />
-         <Result />
-         <BuyCredit />
-      </div>
+      <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/result" element={<Result />} />
+         <Route path="/buy" element={<BuyCredit />} />
+      </Routes>
    );
 };
 
