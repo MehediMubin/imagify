ed8412c8568f4f3f0fd90421477cec5e0cd517be
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-11-14 11:42:15 +0600

    feat: add auth middleware

---
+++ server/middlewares/auth.js
@@ -0,0 +1,24 @@
+const userAuth = async (req, res, next) => {
+   const { token } = req.headers;
+
+   if (!token) {
+      return res.json({
+         success: false,
+         message: "No authentication token, authorization denied",
+      });
+   }
+
+   try {
+      const verified = jwt.verify(token, process.env.JWT_SECRET);
+      req.user.userId = verified.id;
+      next();
+   } catch (error) {
+      console.error("Authentication middleware error:", error);
+      res.json({
+         success: false,
+         message: error.message,
+      });
+   }
+};
+
+export default userAuth;
